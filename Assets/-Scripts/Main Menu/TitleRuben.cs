74c8ec9451c1a8065ae1a63f183782f054a6c180
Corey Abraham <pbv54umpq@relay.firefox.com>
2023-12-13 10:29:54 +1100

    Fixed "SceneController.cs" being inconsistent across scenes + Adjusted Audio Mixing

---
+++ Assets/-Scripts/Main Menu/TitleRuben.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class TitleRuben : MonoBehaviour
+{
+
+}
--- Assets/Audio/Sounds/Ambience/AmbientAudio.cs
+++ Assets/Audio/Sounds/Ambience/AmbientAudio.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 
 public class AmbientAudio : MonoBehaviour
 {
-    [field: SerializeField] private AudioClip[] Clips { get; set; }
-    [field: SerializeField] private float FrequencyMin { get; set; }
-    [field: SerializeField] private float FrequencyMax { get; set; }
-    [field: SerializeField] private float PitchMin { get; set; }
-    [field: SerializeField] private float PitchMax { get; set; }
+    [field: SerializeField] private string[] Clips { get; set; }
     [field: SerializeField] private string AmbienceMusic { get; set; }
+    [field: SerializeField] private Vector2 Frequency { get; set; }
 
-    private void Start()
+    private IEnumerator RandomPlayer()
     {
-        RandomPlayer();
+        float waitTime;
+        while (true)
+        {
+            waitTime = Random.Range(Frequency.x, Frequency.y);
+            yield return new WaitForSeconds(waitTime);
+            PlayRandomSound();
+        }
     }
+
+    private void PlayRandomSound() => AudioManager.Instance.PlaySFX(Clips[Random.Range(0, Clips.Length)]);
+
+    private void Start() => RandomPlayer();
 
     private void Update()
     {
@@ -21,25 +28,5 @@ public class AmbientAudio : MonoBehaviour
             AudioManager.Instance.Play(AmbienceMusic);
         else if (AudioManager.Instance.IsPlaying(AmbienceMusic) && GameManager.Instance.PlayerData.InMenu)
             AudioManager.Instance.Stop(AmbienceMusic);
-    }
-
-    private IEnumerator RandomPlayer()
-    {
-        float waitTime;
-        while (true)
-        {
-            waitTime = Random.Range(FrequencyMin, FrequencyMax);
-            yield return new WaitForSeconds(waitTime);
-            PlayRandomSound();
-        }
-    }
-
-    private void PlayRandomSound()
-    {
-        int soundIdx = Random.Range(0, Clips.Length);
-        float randomPitch = Random.Range(PitchMin, PitchMax);
-
-        //AudioSource.pitch = randomPitch;
-        //AudioManager.Instance.PlaySFX(Clips[soundIdx].name);
     }
 }
